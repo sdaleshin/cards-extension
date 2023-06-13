762ba716c052daf1acf8a91b8d91052fcc508042
sdaleshin <sdaleshin@gmail.com>
2023-06-13 13:38:40 +0200

    added auth

---
+++ scripts/content.js
@@ -0,0 +1,2 @@
+console.log('hi from extension1')
+
---
+++ scripts/oauth.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    document.querySelector('button').addEventListener('click', function() {
+        chrome.identity.getAuthToken({interactive: true}, function(token) {
+            fetch('http://localhost:3000/auth/auth-in-extension', {
+                method: "POST", // *GET, POST, PUT, DELETE, etc.
+                mode: "cors", // no-cors, *cors, same-origin
+                cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: "same-origin", // include, *same-origin, omit
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                redirect: "follow", // manual, *follow, error
+                referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                body: JSON.stringify({
+                    accessToken: token
+                }),
+            }).then(response => response.json()).then(console.log)
+        });
+    });
+};
